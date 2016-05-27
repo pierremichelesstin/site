1183f6a43f85bf5883698d0cc673349979be897f
Pierre Michel <pierre.michouxxx@gmail.com>
2016-05-27 16:24:00 -0500

    Update Bubbles_books_infography_v2.js

--- javascripts/Bubbles_books_infography_v2.js
+++ javascripts/Bubbles_books_infography_v2.js
@@ -21,16 +21,16 @@ d3.json("javascripts/livres2.json", function(data) {
     myAsyncCounter.increment();});
 
 
-//function draw() { //fonction appellee quand le compteur de telechargements atteind la limite definie ligne 14 //efface le truc qui toune
+function draw() { //fonction appellee quand le compteur de telechargements atteind la limite definie ligne 14 //efface le truc qui toune
     //var jsonLivres_lengthb = Object.keys(jsonLivresb).length;
 
-  //  var width = 50,
-  //      height = 50;
+    var width = 50,
+        height = 50;
 
- //   var svg = d3.select("graphe2").append("svg")
- //       .attr("width", width)
-  //      .attr("height", height)
-  //      .style("border", "1px solid black");
+   var svg = d3.select("graphe2").append("svg")
+       .attr("width", width)
+       .attr("height", height)
+       .style("border", "1px solid black");
 
-//}
+}
 
