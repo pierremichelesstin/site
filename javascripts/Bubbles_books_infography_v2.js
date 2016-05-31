0417a19d21796a440e636964a90282198db8cce7
Pierre Michel <pierre.michouxxx@gmail.com>
2016-05-31 15:57:10 -0500

    Update Bubbles_books_infography_v2.js

--- javascripts/Bubbles_books_infography_v2.js
+++ javascripts/Bubbles_books_infography_v2.js
@@ -20,8 +20,8 @@ d3.json("javascripts/livres2.json", function(data) {
     jsonLivresb = data;
     myAsyncCounter.increment();});
 console.log("___________")
-var jsonLivres_length2 = Object.keys(jsonLivresb).length;
-console.log(jsonLivres_length2)
+//var jsonLivres_length2 = Object.keys(jsonLivresb).length;
+console.log(jsonLivres_length)
 console.log("___________")
 
 function draw() { //fonction appellee quand le compteur de telechargements atteind la limite definie ligne 14 //efface le truc qui toune
