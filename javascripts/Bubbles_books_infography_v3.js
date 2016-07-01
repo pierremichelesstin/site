1ffba0e20f41333ea065d7ffaf27810b1f40b445
Pierre Michel <pierre.michouxxx@gmail.com>
2016-07-01 13:09:34 +0200

    Update Bubbles_books_infography_v3.js

--- javascripts/Bubbles_books_infography_v3.js
+++ javascripts/Bubbles_books_infography_v3.js
@@ -49,14 +49,14 @@ var scale_genre = 0;
 var scale_genre_c = 20;
 var clusters_data_temp=[];
 for (i = 0; data_names_subgenre.length > i; i += 1) {
-    //var color_temp=[];
-    //if (data_names_subgenre[i]="Fantasy"){color_temp="rgb(200, 100, 200)";}
-    //else {color_temp="rgb(000, 100, 200)";}
+    var color_temp=[];
+    if (data_names_subgenre[i]="Fantasy"){color_temp="rgb(200, 100, 200)";}
+    else {color_temp="rgb(000, 100, 200)";}
     temp_X={   
             name: data_names_subgenre[i],
             tomes: data_names_subgenre_counter[i],
             radius: data_names_subgenre_counter[i] * scale_genre + scale_genre_c,
-            color: "rgb(000*i, 100, 200)",
+            color: color_temp,
             type: "genre",
             cluster: i};
     clusters_data_temp.push(temp_X);
