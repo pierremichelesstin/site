fcec5d06b5aeeb9b74bd7ffd2c98d1d7785193df
Pierre Michel <pierre.michouxxx@gmail.com>
2016-07-01 12:33:52 +0200

    Update Bubbles_books_infography_v3.js

--- javascripts/Bubbles_books_infography_v3.js
+++ javascripts/Bubbles_books_infography_v3.js
@@ -41,23 +41,25 @@ for (i = 0; Livres.length > i; i += 1) {
   tomes_i=Livres[i].tomes;
 
     var indice_subgenre_i = data_names_subgenre.indexOf(subgenre_i);
-    var indice_subgenre2_i = data_names_subgenre.indexOf(subgenre2_i);
-    
     if(indice_subgenre_i==-1){
         data_names_subgenre.push(subgenre_i);
         data_names_subgenre_counter.push(tomes_i);
     }
-//    else if (indice_subgenre2_i==-1&&subgenre2_i!=undefined){
-//        data_names_subgenre.push(subgenre2_i);
-//        data_names_subgenre_counter.push(tomes_i);
-//    }
     else {
         data_names_subgenre_counter[indice_subgenre_i]=data_names_subgenre_counter[indice_subgenre_i]+tomes_i;
-  //      if (subgenre2_i!=undefined){
-//            data_names_subgenre_counter[indice_subgenre2_i]=data_names_subgenre_counter[indice_subgenre2_i]+tomes_i;
-  //      }
     }
-
+    
+    if(subgenre2_i!=undefined){
+        var indice_subgenre2_i = data_names_subgenre.indexOf(subgenre2_i);
+        if(indice_subgenre2_i==-1){
+            data_names_subgenre.push(subgenre2_i);
+            data_names_subgenre_counter.push(tomes_i);
+        }
+        else {
+            data_names_subgenre_counter[indice_subgenre2_i]=data_names_subgenre_counter[indice_subgenre2_i]+tomes_i;
+        }
+    }
+    
   if (Livres[i].subgenre == "Science-fiction") {
     subgenre1_size = subgenre1_size + Livres[i].tomes;
   } else if (Livres[i].subgenre == "Space-op\xE9ra") {
