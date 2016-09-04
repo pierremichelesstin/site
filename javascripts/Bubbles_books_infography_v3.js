332e28d1677cec6d018c72d561a44fb90e4c1569
Pierre Michel <pierre.michouxxx@gmail.com>
2016-09-04 18:47:14 +0200

    Update Bubbles_books_infography_v3.js

--- javascripts/Bubbles_books_infography_v3.js
+++ javascripts/Bubbles_books_infography_v3.js
@@ -49,7 +49,7 @@ function draw() { //Fonction appellee quand le compteur de telechargements attei
     }
 
     var width = 1000,
-        height = 575,
+        height = 650,
         padding = 4, // separation between same-color circles
         clusterPadding = 6; // separation between different-color circles
 
